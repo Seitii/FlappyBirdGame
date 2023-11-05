977d5287d8cb8153158b5c3c082240e77cd13344
Seitii <hugoseite@icloud.com>
2023-11-05 16:42:12 -0300

    FlappyBird Game

---
+++ FlappyBird.py
@@ -0,0 +1,328 @@
+import pygame
+import os
+import random
+import neat
+
+ai_jogando = True
+geracao = 0
+
+# CONSTRUÇÃO DO JOGO
+
+TELA_LARGURA = 580
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 30)
+
+
+# CRIAR AS COiSAS QUE SE MOVIMENTAM
+
+class Passaro:
+    # DEFININDO ATRIBUTOS E METODOS
+    IMGS = IMAGENS_PASSARO
+
+    # animação da rotação
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    # ATRIBUTOS DO PASSARO
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0  # TEMPO DA ANIMAÇÃO DO PASSARO NO MOVIMENTO
+        self.contagem_imagem = 0
+        self.imagem = self.IMGS[0]
+
+    def pular(self):  # X é NEGATIVO
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # CALCULAR O DESCOLAMENTO
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo ** 2) + self.velocidade * self.tempo
+
+        # RESTRINGIR O DESLOCAMENTO
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+        self.y += deslocamento
+
+        # ANGULO DO PASSAR0 (ANIMAÇÃO)
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > - 90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+
+    def desenhar(self, tela):
+        # DEFINIR A IMAGEM DO PASSARO
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]  # ASA PRA CIMA
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 2:
+            self.imagem = self.IMGS[1]  # ASA PRO MEIO
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 3:
+            self.imagem = self.IMGS[2]  # ASA PRA BAIXO
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 4:
+            self.imagem = self.IMGS[1]  # ASA PRA MEIO
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO * 4 + 1:
+            self.imagem = self.IMGS[0]  # ASA PRA BAIXO
+            self.contagem_imagem = 0  # ZERA A IMAGEM
+
+        # SE O PASSARO CAIR, NAO BATER ASA
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO * 2
+
+        # DESENHAR A IMAGEM
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        posicao_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center  # CRIANDO O RETANGULO DA IMAGEM
+        retangulo = imagem_rotacionada.get_rect(center=posicao_centro_imagem)  # DESENHA O CENTRO DA IMAGEM
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        # IDENTIFICAR A COLISAO DOS OBJETOS
+        return pygame.mask.from_surface(self.imagem)  # PEGANDO A MASCARA DO PASSARO
+
+
+class Cano:
+    # PARAMETROS
+    DISTANCIA = 200
+    VELOCIDADE = 10
+
+    def __init__(self, x):
+        self.x = x
+        # ALTURA DE 1 CANO
+        self.altura = 0
+        self.pos_topo = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        # IDENFICAR SE O PASSARO PASSOU DO CANO
+        self.passou = False
+        # GERA O VALOR DA ALTURA DO CANO
+        self.definir_altura()
+
+    def definir_altura(self):
+        # GERA UM NUMERO ALEATORIO DENTRO DESSA MARGEM
+        self.altura = random.randrange(50, 450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base = self.altura + self.DISTANCIA
+
+    # MOVER O CANO
+    def mover(self):
+        self.x -= self.VELOCIDADE # MOVE O CANO DE FORMA NEGATIVA
+
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.pos_base))
+
+    # COLISÃO
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+        distancia_topo = (self.x - passaro.x, self.pos_topo - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.pos_base - round(passaro.y))
+
+        # VERIFICA O PONTO DE COLISÃO
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
+
+
+class Chao:
+    VELOCIDADE = 5
+    LARGURA = IMAGEM_CHAO.get_width()  # PEGA A LARGURA DA IMAGEM DO CHAO
+    IMAGEM = IMAGEM_CHAO
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.x1 + self.LARGURA
+
+    def mover(self):
+        self.x1 -= self.VELOCIDADE
+        self.x2 -= self.VELOCIDADE
+
+        # MOVIMENTO DO CHAO
+        if self.x1 + self.LARGURA < 0:
+            self.x1 = self.x2 + self.LARGURA
+        if self.x2 + self.LARGURA < 0:
+            self.x2 = self.x1 + self.LARGURA
+
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x2, self.y))
+        tela.blit(self.IMAGEM, (self.x1, self.y))
+
+
+# DESENHAR O JOGO
+def desenhar_tela(tela, passaros, canos, chao, pontos):
+    tela.blit(IMAGEM_BACKGROUND, (0, 0))
+    for passaro in passaros:
+        passaro.desenhar(tela)
+    for cano in canos:
+        cano.desenhar(tela)
+
+    texto = FONTE_PONTOS.render(f"Pontuacao: {pontos}", 1, (255, 255, 255))
+    tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))  # COLOCAR O TEXTO NA ALTURA E LARGURA
+
+    if ai_jogando:
+        texto = FONTE_PONTOS.render(f"Geração: {geracao}", 1, (255, 255, 255))
+        tela.blit(texto, (10, 10))  # COLOCAR O TEXTO NA ALTURA E LARGURA
+
+    chao.desenhar(tela)
+    pygame.display.update()
+
+
+def main(genomas, config):
+    # FITNESS FUNCTION == QUAL PASSARO FOI MELHOR OU PIOR
+    global geracao
+    geracao += 1
+
+    if ai_jogando:
+        # CRIAÇÃO DE VARIOS PASSAROS
+        redes = []
+        lista_genomas = []
+        passaros = []
+        for _, genoma in genomas:
+            rede = neat.nn.FeedForwardNetwork.create(genoma, config)  # METODO PARA CRIAR A REDE NEURAL
+            redes.append(rede)
+            genoma.fitness = 0  # PONTUAÇÃO DO PASSARO, RECOMPENSA PRO PASSARO.
+            lista_genomas.append(genoma)
+            passaros.append(Passaro(230, 350))
+    else:
+        passaros = [Passaro(230, 350)]
+
+    chao = Chao(730)
+    canos = [Cano(700)]
+    tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+    pontos = 0
+    relogio = pygame.time.Clock()  # tempo q atualiza a tela
+
+    # COLOCANDO O JOGO PRA FUNCIONAR
+    rodando = True
+    while rodando:
+        relogio.tick(30)
+
+        # INTERAÇÃO DO USUARIO COM O JOGO
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                rodando = False
+                pygame.quit()
+                quit()
+            if not ai_jogando:
+                if evento.type == pygame.KEYDOWN:  # EVENTO DE APERTAR UMA TECLA NO TECLADO
+                    if evento.key == pygame.K_SPACE:
+                        for passaro in passaros:
+                            passaro.pular()
+        indice_cano = 0
+        if len(passaros) > 0:
+            # DESCOBRIR QUAL CANO O PASSARO OLHA
+            if len(canos) > 1 and passaros[0].x > (canos[0].x + canos[0].CANO_TOPO.get_width()):    
+                indice_cano = 1
+        else:
+            rodando = False
+            break
+
+        # MOVER AS COISAS
+        for i, passaro in enumerate(passaros):
+            passaro.mover()
+
+            # AUMENTAR A FITNESS DO PASSARO
+            lista_genomas[i].fitness += 0.1
+
+            # INFORMAÇÃO SE O PASSARO PULA OU NAO
+            output = redes[i].activate((passaro.y,
+                                        abs(passaro.y - canos[indice_cano].altura),
+                                        abs(passaro.y - canos[indice_cano].pos_base)))
+            if output[0] > 0.5:
+                passaro.pular()
+
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+        for cano in canos:
+            for i, passaro in enumerate(passaros):  # pega o passaro dentro DA LISTA
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                    if ai_jogando:
+                        lista_genomas[i].fitness -= 1
+                        lista_genomas.pop(i)
+                        redes.pop(i)
+                if not cano.passou and passaro.x > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+
+
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano)
+
+        if adicionar_cano:
+            pontos += 1
+            canos.append(Cano(600))
+
+            for genoma in lista_genomas:
+                genoma.fitness += 5
+        for cano in remover_canos:
+            canos.remove(cano)
+
+        if pontos >= 5:
+            cano.VELOCIDADE = 15
+
+        for i, passaro in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
+                if ai_jogando:
+                    lista_genomas.pop(i)
+                    redes.pop(i)
+
+        desenhar_tela(tela, passaros, canos, chao, pontos)
+
+
+def rodar(caminho_config):
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                caminho_config)
+
+    populacao = neat.Population(config)
+    populacao.add_reporter(neat.StdOutReporter(True))
+    populacao.add_reporter(neat.StatisticsReporter())
+
+    if ai_jogando:
+        populacao.run(main, 100)
+    else:
+        main(None, None)
+
+
+if __name__ == '__main__':
+    caminho = os.path.dirname(__file__)
+    caminho_config = os.path.join(caminho, 'config.txt')
+    rodar(caminho_config)
